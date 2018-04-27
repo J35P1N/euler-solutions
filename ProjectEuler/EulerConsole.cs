6a4bb8dcc7394c2dfe34262a9e4051d17043c1b2
Jason Espin <jespin@emea.corp.jwt.com>
2018-04-27 14:58:24 +0100

    Solution 1 completed

---
+++ ProjectEuler/EulerConsole.cs
@@ -0,0 +1,44 @@
+﻿using ProjectEuler.Problems;
+
+namespace ProjectEuler
+{
+    using System;
+
+    class EulerConsole
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                ;
+                Console.WriteLine("Welcome to the Euler test suite.");
+                Console.WriteLine("Enter a problem number to generate the correct answer or press Esc to exit:");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+                string decision = Console.ReadLine();
+                if (int.TryParse(decision, out int iDecision))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (iDecision)
+                    {
+                        case 1:
+                            new MultiplesOfThreeAndFive();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid entry. Please try another.");
+                        break;
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press any key to continue..");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ ProjectEuler/Problems/MultiplesOfThreeAndFive.cs
@@ -0,0 +1,38 @@
+﻿namespace ProjectEuler.Problems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MultiplesOfThreeAndFive
+    {
+        public MultiplesOfThreeAndFive()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Question 1:");
+            Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.");
+            Console.WriteLine("Find the sum of all the multiples of 3 or 5 below 1000.");
+
+
+            var numberArray = new int[] {3, 5};
+            var multiplesList = new List<int>();
+            for (int i = 1; i < 1000; i++)
+            {
+                foreach (var n in numberArray)
+                {
+                    // Modulus allows us to see if a number is divisible by another number therefore indicating
+                    // if it is a multiple
+                    if (i % n == 0)
+                    {
+                        multiplesList.Add(i);
+                    }
+                }
+            }
+
+            var result = multiplesList.Distinct().Sum();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(result);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
